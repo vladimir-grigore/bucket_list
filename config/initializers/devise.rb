5450a91f8366769fc5500066627b8612a5eb2203
Vladimir Grigore <vladimir.grigore@gmail.com>
2024-01-31 16:47:57 -0800

    Use .env file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,7 +313,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = '1234' # ENV["DEVISE_JWT_SECRET_KEY"]
+    jwt.secret = ENV["DEVISE_JWT_SECRET_KEY"]
     jwt.dispatch_requests = [
       ['POST', %r{^/users/sign_in$}]
     ]
